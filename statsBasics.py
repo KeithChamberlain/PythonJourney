7b0b14ebf560ec163fc9ee5638f9a908f02e9503
KeithChamberlain <kchamberln@gmail.com>
2021-04-25 02:50:03 -0600

    Update statsBasics.py

--- statsBasics.py
+++ statsBasics.py
@@ -1,2 +1,3 @@
 import numpy as np # for mean, average nanmean, median, percentile, var & std (with ddof=1 or 0: Bessels correction)
 from scipy import stats # for mode & more
+import math # for factorial (or create own). 
